cb65c539ae6918565caa0204e8df1d7c4732b614
Lilian <117281250+Equinox7777@users.noreply.github.com>
2023-11-02 19:52:31 +0100

    Update conWD.cpp

--- conWD.cpp
+++ conWD.cpp
@@ -1,6 +1,7 @@
 #include "conWD.h"
-#include <iostream>
 #include "SDL_ttf.h"
+
+#include <iostream>
 
 void ConWD::showPopup(const std::string& title, const std::string& message)
 {
@@ -24,23 +25,24 @@ void ConWD::showPopup(const std::string& title, const std::string& message)
 
 bool ConWD::askForReplay(const std::string& title, const std::string& message)
 {
-    const SDL_MessageBoxButtonData buttons[] = {
+    const SDL_MessageBoxButtonData buttons[] = 
+    {
         { SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT, 0, "Oui" },
         { SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT, 1, "Non" }
     };
 
-    // Créez une petite fenêtre SDL temporaire
     SDL_Window* tempWindow = SDL_CreateWindow(
         "TempWindow",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        100, 100, // Définissez la taille que vous souhaitez
-        SDL_WINDOW_HIDDEN // Cachez la fenêtre temporaire
+        100, 100,
+        SDL_WINDOW_HIDDEN
     );
 
-    SDL_MessageBoxData messageboxdata = {
+    SDL_MessageBoxData messageboxdata = 
+    {
         SDL_MESSAGEBOX_INFORMATION,
-        tempWindow, // Utilisez la fenêtre temporaire
+        tempWindow,
         title.c_str(),
         message.c_str(),
         SDL_arraysize(buttons),
@@ -51,7 +53,6 @@ bool ConWD::askForReplay(const std::string& title, const std::string& message)
     int buttonId;
     SDL_ShowMessageBox(&messageboxdata, &buttonId);
 
-    // Détruisez la fenêtre temporaire
     SDL_DestroyWindow(tempWindow);
 
     if (buttonId == 0)
