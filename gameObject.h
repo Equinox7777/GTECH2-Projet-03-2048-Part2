db233072f7bfbc7964f0a5c34cae7e4388a2e690
Lilian <117281250+Equinox7777@users.noreply.github.com>
2023-11-02 19:53:36 +0100

    Update gameObject.h

--- gameObject.h
+++ gameObject.h
@@ -1,16 +1,18 @@
 #pragma once
 #include "SDL.h"
+
 #include <string>
 #include <vector>
 
-enum class AnimationType {
+enum class AnimationType 
+{
     None,
     Upgrade,
 };
 
 class GameObject
 {
-    
+
 public:
     GameObject();
 
@@ -21,7 +23,6 @@ public:
 
     SDL_Color m_color;
 
-//    void runAnimation(AnimationType animationType, int x, int y);
     int setPosition(int x, int y);
     int getX();
     int getY();
